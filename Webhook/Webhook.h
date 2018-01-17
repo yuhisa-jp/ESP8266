721904048567789a2d9214c049dad866a68027e4
yuhisa-jp <yuhisa@gmail.com>
2018-01-17 19:17:16 +0900

    Update Webhook.h

--- Webhook/Webhook.h
+++ Webhook/Webhook.h
@@ -1,7 +1,7 @@
 /**
  *
  * @file Webhook.h
- * @date 07.01.2018
+ * @date 17.01.2018
  * @author yuhisa
  * This file is distributed under GNU Lesser General Public License v2.1.
  */
@@ -22,12 +22,13 @@ class WebhookClass
 {
 
   private:
-    String WebhookHost, WebhookEndpoint;
-    String WebhookContentType = "application/x-www-form-urlencoded";
+    char* WebhookHost;
+    char* WebhookEndpoint;
+    char WebhookContentType[64] = "application/x-www-form-urlencoded";
   
   public:
-    void setHost (const char* host, const char* endpoint);
-    void setContentType (const char* type);
+    void setHost (char* host, char* endpoint);
+    void setContentType (char* type);
     bool post (String data);
     
 };
